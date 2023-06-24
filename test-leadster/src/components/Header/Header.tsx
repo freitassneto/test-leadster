115c4bc737c94ae99228cb82265dcab57b288160
Olivio Freitas <ojfneto92@gmail.com>
2023-06-24 11:39:02 -0300

    feat: adjust header logo

--- test-leadster/src/components/Header/Header.tsx
+++ test-leadster/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HeaderStyled } from "./HeaderStyled";
 import Image from "next/image";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/images/logo.png"
 
 export const Header = () => {
   return (
--- test-leadster/src/pages/index.tsx
+++ test-leadster/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Header } from "@/components/Header/Header";
+import { BigBanner } from "@/components/BigBanner/BigBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <BigBanner/>
     </>
   );
 }
