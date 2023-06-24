ca59d43960a7e4b1ae3001b2e9aaec8a7ecbcac8
Olivio Freitas <ojfneto92@gmail.com>
2023-06-24 10:55:28 -0300

    feat: implement global styles

---
+++ test-leadster/src/styles/GlobalStyle.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+   :root {
+    --color-black: #000000;
+    --color-white: #FFFFFF;
+
+    --color-dark-blue: #1c3c50;
+    --color-blue-300: #1f76f0;
+    --color-blue-200: #2c83fb;
+    --color-blue-100: #f0f8ff;
+
+    font-size: 60%;   
+  }
+
+  @media (min-width: 700px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+  
+  * {
+    margin:0;
+    padding: 0;
+    outline:0;
+    box-sizing: border-box;
+  }
+
+  body,html{
+    width: 100vw;
+    height: 100vh;
+  }
+
+  body {
+    background: var(--color-gray-900);
+    color: var(--color-gray-300);
+    -webkit-font-smoothing: antialiased;
+
+    overflow-x: hidden;
+  }
+
+  body, input, button, textarea {
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-size: 1.6rem;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
